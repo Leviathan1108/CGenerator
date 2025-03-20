1f3b87c8c8aaa3bb17c60d12efff6c1b70ef30d9
Leviathan1108 <lutfifuadradityawan@gmail.com>
2025-03-20 14:21:47 +0700

    Renovasi by luthfi

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,2 @@
+@extends('layout.v_layout')
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CertiController;
+use App\Http\Controllers\TemplatesController;
+use App\Http\Controllers\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/certificate', [CertiController::class, 'index']);
+Route::get('/settings', [SettingsController::class, 'index']);
+Route::get('/templates', [TemplatesController::class, 'index']);
