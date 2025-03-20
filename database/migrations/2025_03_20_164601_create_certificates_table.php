8c16d2cfa4cbc25572b8f4124d6f3649abc41b66
athikah27 <athikayou@gmail.com>
2025-03-21 00:52:25 +0700

    tabel database
    tabel untuk certificate, recipient, templates, dan verification pada database tinggal Jalankan "php artisan migrate" untuk membuat tabel.

---
+++ database/migrations/2025_03_20_164601_create_certificates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+{
+    Schema::create('certificates', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('template_id')->constrained('templates')->onDelete('cascade');
+        $table->foreignId('recipient_id')->constrained('recipients')->onDelete('cascade');
+        $table->foreignId('issued_by')->nullable()->constrained('users')->onDelete('set null');
+        $table->date('issue_date')->default(now());
+        $table->enum('status', ['pending', 'sent', 'verified'])->default('pending');
+        $table->string('verification_code')->unique();
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}
---
+++ database/migrations/2025_03_20_164606_create_verifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVerificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('certificate_id')->constrained('certificates')->onDelete('cascade');
+            $table->timestamp('verified_at')->default(now());
+            $table->string('verified_by');
+            $table->timestamps();
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verifications');
+    }
+}
