c088ff559cb43754f983374b8a0bd1d1c9989d2e
Chay-DevOP <nc951422@gmail.com>
2025-04-10 23:07:31 +0700

    edit tampilan
    menambahkan tag html dan body, supaya tampilan email.blade.php tidak berada didalam halaman dashboard.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,23 +1,34 @@
-@extends('layout.v_layout')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-    <div class="container d-flex align-items-center justify-content-center" style="height: 100vh; background-color: #f8f9fa;">
-        <div class="d-flex bg-light rounded-4 overflow-hidden" style="max: width 800px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
-            <div class="text-light d-flex flex-column justify-content-center align-items-center text-center"
-                style="padding: 20px; width: 40%; background-color: #232e66;">
-                <div class="fw-bold fs-4 mb-3">Certificate Generator</div>
-                <div class="d-flex align-items-center justify-content-center fs-3 fw-bold rounded-2 mb-3"
-                    style="color: #000; width: 80px; height: 80px; background-color: #fbb041;">CG</div>
-                <div class="fw-bold text-center">WELCOME TO <br> CERTIFICATE GENERATOR</div>
-                <p class="text-warning fs-9">Create, manage, and verify certificates</p>
-            </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Certificate Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 
-            <!-- form atau content kiri -->
-            <div style="width: 60%; padding: 40px; background-color: #f8f9fa;">
+</head>
+
+<body class="container d-flex align-items-center justify-content-center"
+    style="height: 100vh; background-color: #f8f9fa;">
+    <div class="d-flex bg-light overflow-hidden"
+        style="border-radius: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
+        <div class="text-light d-flex flex-column justify-content-center align-items-center text-center"
+            style="padding: 20px; width: 40%; background-color: #232e66;">
+            <div class="fw-bold fs-4 mb-3">Certificate Generator</div>
+            <div class="d-flex align-items-center justify-content-center fs-3 fw-bold rounded-2 mb-3"
+                style="color: #000; width: 80px; height: 80px; background-color: #fbb041;">CG</div>
+            <div class="fw-bold text-center">WELCOME TO <br> CERTIFICATE GENERATOR</div>
+            <p class="text-warning fs-9">Create, manage, and verify certificates</p>
+        </div>
+
+        <!-- form atau content kiri -->
+        <div style="width: 60%; padding: 40px; background-color: white;">
             @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
             @endif
             <h3 class="mb-3 text-center fw-bold" style="color: blue;">Reset Password</h3>
             <p class="text-start">Enter your email address and we'll send you <br> a link to reset your password.</p>
@@ -38,15 +49,17 @@
                     </div>
                 </div>
                 <div class="d-flex justify-content-center align-items-center mb-3">
-                        <button type="submit" class="btn fw-bold" style="background-color: #fbb041; width: 200px;">
-                            {{ __('Send Reset Link') }}
-                        </button>
+                    <button type="submit" class="btn fw-bold" style="background-color: #fbb041; width: 200px;">
+                        {{ __('Send Reset Link') }}
+                    </button>
                 </div>
             </form>
             <div class="mt-3 text-center">
-                <p>Remember your password?<br><a href="/login" class="text-decoration-none fw-bold">Back to login </a></p>
-            </div>
+                <p>Remember your password?<br><a href="/login" class="text-decoration-none fw-bold">Back to login </a>
+                </p>
             </div>
         </div>
     </div>
-@endsection
+</body>
+
+</html>
